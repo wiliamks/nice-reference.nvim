224f8dde880ef8490217b6d1e752eedb099ec172
Wiliam Otto <wiliam.keltke@gmail.com>
2025-04-19 12:18:46 -0300

    add close mappings to preview window

--- lua/nice-reference/actions.lua
+++ lua/nice-reference/actions.lua
@@ -5,73 +5,88 @@ local api = vim.api
 local util = require 'vim.lsp.util'
 
 M.jump = function(item, encoding)
-	if item == nil then
-		return
-	end
+    if item == nil then
+        return
+    end
 
-	local location = {
-		uri = "file://" .. item.filename,
-		range = {
-			start = {
-				line = item.lnum - 1,
-				character = item.col - 1
-			}
-		}
-	}
+    local location = {
+        uri = "file://" .. item.filename,
+        range = {
+            start = {
+                line = item.lnum - 1,
+                character = item.col - 1
+            }
+        }
+    }
 
-	util.show_document(location, encoding, { focus = true })
+    util.show_document(location, encoding, { focus = true })
 end
 
 M.close = function(_, _, _)
-	vim.api.nvim_win_close(0, true)
+    vim.api.nvim_win_close(0, true)
 end
 
 M.choose = function(_, item, encoding)
-	M.close()
-	M.jump(item, encoding)
+    M.close()
+    M.jump(item, encoding)
 end
 
 M.preview = function(_, item, _)
-	local ok, lib = pcall(require, 'goto-preview.lib')
-	if ok then
-		M.close()
-		lib.open_floating_win('file://' .. item.filename, { item.lnum, item.col - 1 })
-	else
-		vim.notify("goto-preview not installed")
-	end
+    local ok, lib = pcall(require, 'goto-preview.lib')
+    if ok then
+        M.close()
+        local preview_window = lib.open_floating_win('file://' .. item.filename, { item.lnum, item.col - 1 })
+
+        local function map_close_window(keys)
+            for i = 1, #keys do
+                vim.keymap.set(
+                    "n",
+                    keys[i],
+                    function()
+                        vim.api.nvim_win_close(preview_window, true)
+                    end,
+                    { silent = true, noremap = true, buffer = vim.api.nvim_win_get_buf(preview_window) }
+                )
+            end
+        end
+
+        map_close_window({ '<C-c>', 'q' })
+    else
+        vim.notify("goto-preview not installed")
+    end
 end
 
 M.move_to_quick_fix = function(items, _, _)
-	M.close()
-	local title = 'References'
-	vim.fn.setloclist(0, {}, ' ', { title = title, items = items })
+    M.close()
+    local title = 'References'
+    vim.fn.setloclist(0, {}, ' ', { title = title, items = items })
     api.nvim_command('lopen')
 end
 
 M.open_on_new_tab = function(_, item, encoding)
-	M.close()
-	vim.cmd('tabedit')
-	M.jump(item, encoding)
+    M.close()
+    vim.cmd('tabedit')
+    M.jump(item, encoding)
 end
 
 M.open_vsplit = function(_, item, encoding)
-	M.close()
-	vim.cmd('vsplit')
-	M.jump(item, encoding)
+    M.close()
+    vim.cmd('vsplit')
+    M.jump(item, encoding)
 end
 
 M.open_split = function(_, item, encoding)
-	M.close()
-	vim.cmd('split')
-	M.jump(item, encoding)
+    M.close()
+    vim.cmd('split')
+    M.jump(item, encoding)
 end
 
 M.next = function(_, _, _)
-	vim.cmd('normal! j')
+    vim.cmd('normal! j')
 end
 
 M.previous = function(_, _, _)
-	vim.cmd('normal! k')
+    vim.cmd('normal! k')
 end
 
 return M
